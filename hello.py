c6f67d9ade18e42aa4b8b834f737761b8670d774
Alice Tweedy McGrath <atmcgrath@gmail.com>
2025-03-21 17:08:07 -0400

    added custom header to hello

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p><p><a href='https://support.reclaimhosting.com/hc/en-us/articles/9220813439127'>More info</a></p>"
+    return "<h1>One thousand billion Bryn Mawrs</h1><p>Hello, World!</p><p><a href='https://support.reclaimhosting.com/hc/en-us/articles/9220813439127'>More info</a></p>"
